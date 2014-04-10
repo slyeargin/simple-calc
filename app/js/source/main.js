745ce7f6a64235181dcd513569c4e2b0340ccdb5
Samantha Yeargin <slyeargin@gmail.com>
2014-04-10 16:04:26 -0500

    Added subtraction, multiplication and division functions

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -4,15 +4,45 @@
   $(document).ready(initialize);
 
   function initialize(){
-    $('#btn').click(findSum);
+    $('#btnAdd').click(add);
+    $('#btnSub').click(subtract);
+    $('#btnMul').click(multiply);
+    $('#btnDiv').click(divide);
   }
 
-  function findSum(){
-    var x = $('#num1').val();
-    var y = $('#num2').val();
-    $('#sum').text(parseInt(x)+parseInt(y));
+  function add() {
+    var values = getNumbers();
+    var sum = values[0] + values[1];
+    display(sum);
   }
 
+  function subtract() {
+    var values = getNumbers();
+    var difference = values[0] - values[1];
+    display(difference);
+  }
+
+  function multiply() {
+    var values = getNumbers();
+    var product = values[0] * values[1];
+    display(product);
+  }
+
+  function divide() {
+    var values = getNumbers();
+    var quotient = values[0] / values[1];
+    display(quotient);
+  }
+
+  function getNumbers(){
+    var x = $('#num1').val() * 1;
+    var y = $('#num2').val() * 1;
+    return [x, y];
+  }
+
+  function display(answer){
+  $('#answer').text(answer);
+  }
 
 
 })();
